1c5f20656357b02b02ca37f0563e794e7a08b398
ivancernja <ivancernja@gmail.com>
2017-04-23 14:45:48 +0200

    added dataset array

---
+++ npm-stats/src/components/LineChart.vue
@@ -0,0 +1,51 @@
+<script>
+props: {
+	chartData: {
+		type: Array,
+		required: false
+	},
+	chartLabels: {
+		type: Array,
+		required: true
+	}
+},
+data () {
+	return {
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					},
+					gridLines: {
+						display: true
+					}
+				}],
+				xAxes: [{
+					gridLines: {
+						display: false
+					}
+				}]
+			},
+			responsive: true,
+			maintainAspectRatio: false
+		}
+	}
+},
+mounted () {
+	this.renderChart({
+	labels: this.chartLabels,
+	datasets: [
+	{
+		label: 'downloads',
+		borderColor: '#249EBF',
+		pointBackgroundColor: 'white',
+		borderWidth: 1,
+		pointBorderColor: '#249EBF'
+		backgroundColor: 'transparent'
+		data: this.chartData
+	}
+	], this.options)
+}
+})
+</script>
